a7f4eda9ac557f0cb97c0d4ea2105d0228b6008b
KMalone42 <kmalone42@outlook.com>
2026-02-17 16:49:39 -0500

    fixed up preload while adding the prependPrompt and appendPrompt settings fields

--- src/preload.cjs
+++ src/preload.cjs
@@ -42,47 +42,13 @@ contextBridge.exposeInMainWorld('settings', {
   setAutoSave: (autoSave) => ipcRenderer.invoke('settings:setAutoSave', autoSave),
   getAnalytics: () => ipcRenderer.invoke('settings:getAnalytics'),
   setAnalytics: (analytics) => ipcRenderer.invoke('settings:setAnalytics', analytics),
+    getPrependPrompt: () => ipcRenderer.invoke('settings:getPrependPrompt'),
+    setPrependPrompt: (prependPrompt) => ipcRenderer.invoke('settings:setPrependPrompt', prependPrompt),
+    getAppendPrompt: () => ipcRenderer.invoke('settings:getAppendPrompt'),
+    setAppendPrompt: (appendPrompt) => ipcRenderer.invoke('settings:setAppendPrompt', appendPrompt),
   getMaxTokens: () => ipcRenderer.invoke('settings:getMaxTokens'),
   setMaxTokens: (maxTokens) => ipcRenderer.invoke('settings:setMaxTokens', maxTokens),
   getTimeout: () => ipcRenderer.invoke('settings:getTimeout'),
   setTimeout: (timeout) => ipcRenderer.invoke('settings:setTimeout', timeout),
   resetToDefaults: () => ipcRenderer.invoke('settings:resetToDefaults'),
 });
-        //// Ensure we have access to ipcRenderer
-        //const { ipcRenderer } = require('electron');
-//
-        //// Make window.settings exist
-        //window.settings = {
-          //getTheme: () => ipcRenderer.invoke('settings:getTheme'),
-          //setTheme: (v) => ipcRenderer.invoke('settings:setTheme', v),
-//
-          //getLanguage: () => ipcRenderer.invoke('settings:getLanguage'),
-          //setLanguage: (v) => ipcRenderer.invoke('settings:setLanguage', v),
-//
-          //getModel: () => ipcRenderer.invoke('settings:getModel'),
-          //setModel: (v) => ipcRenderer.invoke('settings:setModel', v),
-//
-          //getTemperature: () => ipcRenderer.invoke('settings:getTemperature'),
-          //setTemperature: (v) => ipcRenderer.invoke('settings:setTemperature', v),
-//
-          //getSaveHistory: () => ipcRenderer.invoke('settings:getSaveHistory'),
-          //setSaveHistory: (v) => ipcRenderer.invoke('settings:setSaveHistory', v),
-//
-          //getAutoSave: () => ipcRenderer.invoke('settings:getAutoSave'),
-          //setAutoSave: (v) => ipcRenderer.invoke('settings:setAutoSave', v),
-//
-          //getAnalytics: () => ipcRenderer.invoke('settings:getAnalytics'),
-          //setAnalytics: (v) => ipcRenderer.invoke('settings:setAnalytics', v),
-//
-          //getOllamaBaseUrl: () => ipcRenderer.invoke('settings:getOllamaBaseUrl'),
-          //setOllamaBaseUrl: (v) => ipcRenderer.invoke('settings:setOllamaBaseUrl', v),
-//
-          //getMaxTokens: () => ipcRenderer.invoke('settings:getMaxTokens'),
-          //setMaxTokens: (v) => ipcRenderer.invoke('settings:setMaxTokens', v),
-//
-          //getTimeout: () => ipcRenderer.invoke('settings:getTimeout'),
-          //setTimeout: (v) => ipcRenderer.invoke('settings:setTimeout', v),
-//
-          //resetToDefaults: () => ipcRenderer.invoke('settings:resetToDefaults'),
-        //};
-//
